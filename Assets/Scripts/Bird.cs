d1f362fb4eb32a31fef9b2822c2db36e2c077212
Pranav Krishnamurthy <pkrish19@outlook.com>
2017-05-18 19:55:13 -0400

    Up-to-date
    Granth is now up-to-date

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+
+    private bool isDead = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+
+            }
+        }
+    }
+}
