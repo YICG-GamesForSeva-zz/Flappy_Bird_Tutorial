8b7122b1fc72395aeaed562a8b9d0f6308d9234f
Pranav Krishnamurthy <pkrish19@outlook.com>
2017-05-18 20:31:24 -0400

    Adding animations
    Animations for the bird, starting with the Idle animation

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-
+    public float upForce = 200f;
     private bool isDead = false;
+    private Rigidbody2D rb2d;
 
     // Use this for initialization
     void Start()
     {
-
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,8 +21,14 @@ public class Bird : MonoBehaviour
         {
             if(Input.GetMouseButtonDown(0))
             {
-
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
             }
         }
+    }
+
+    void OnCollisionEnter2D()
+    {
+        isDead = true;
     }
 }
